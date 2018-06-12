fdb97f26233d4dc1d03901b3bb9557a807e5fb65
Tiago Donizete dos Santos <tiago.santos@mtpi.com.br>
2018-06-12 14:15:13 -0300

    app inicial (erro ao carregar template)

--- systemjs.config.js
+++ systemjs.config.js
@@ -20,7 +20,7 @@
         packages: {
             app: {
                 main: './main.js',
-                defaultExtension: '.js'
+                defaultExtension: 'js'
             },
             rxjs: {
                 defaultExtension: 'js'
