003fd2f99f7fed749068e4b173ffc255ef7cc1c6
Tiago Donizete dos Santos <tiago.santos@mtpi.com.br>
2018-06-13 10:05:31 -0300

    projeto inicial funcionando

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App [PJN}</h1>'
+    template: `<h3>Minha primeira aplicação Angular 2.0 no VS Code </h3><br>Macoratti .net
+        <div class="alert alert-success" role="alert">
+          <strong>Minha primeira aplicação Angular 2.0  no VS Code - </strong>Macoratti .net.
+        </div>
+    `
 })
-
-export class AppComponent {}
+export class AppComponent { }  
